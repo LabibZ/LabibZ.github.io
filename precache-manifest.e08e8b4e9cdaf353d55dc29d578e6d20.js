cf5c997513cc5515df11c31ee44b33c13ca6fe70
Labib <labib.zaman@mail.utoronto.ca>
2023-01-18 23:47:57 -0500

    Updates

---
+++ precache-manifest.e08e8b4e9cdaf353d55dc29d578e6d20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68d3d2d5f77edf7496b706f928c0a859",
+    "url": "/labibz.github.io/index.html"
+  },
+  {
+    "revision": "d375eb0d42c327f97812",
+    "url": "/labibz.github.io/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "86e6a1f081dd65845b9b",
+    "url": "/labibz.github.io/static/css/main.4127d54c.chunk.css"
+  },
+  {
+    "revision": "d375eb0d42c327f97812",
+    "url": "/labibz.github.io/static/js/2.c4470e86.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/labibz.github.io/static/js/2.c4470e86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86e6a1f081dd65845b9b",
+    "url": "/labibz.github.io/static/js/main.85c7eb7a.chunk.js"
+  },
+  {
+    "revision": "660e8535a728d477334b",
+    "url": "/labibz.github.io/static/js/runtime-main.1fc2a89c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2f92edaabcf6d7b44af66665cf08006c.js"
+  "/labibz.github.io/precache-manifest.e08e8b4e9cdaf353d55dc29d578e6d20.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/labibz.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
